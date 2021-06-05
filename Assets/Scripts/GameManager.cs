fa383de7091cff4dbca09fa900823dcf8bfcbcf7
Mari <marwalsh@uat.edu>
2021-06-04 23:09:09 -0700

    Begun Title Screen

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    // Run right before Start()
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("Attempted to create a second GameManager");
+            Destroy(this);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainmenu;
+    public GameObject optionsmenu;
+    public void QuitGame()
+    {
+        Debug.Log("Quitting the game");
+        Application.Quit();
+    }
+
+    public void DisplayOptionsMenu()
+    {
+        optionsmenu.SetActive(true);
+        mainmenu.SetActive(false);
+    }
+
+    public void CloseOptionMenu()
+    {
+        mainmenu.SetActive(true);
+        optionsmenu.SetActive(false);
+    }
+
+    public void StartNewGame()
+    {
+        //SceneManager.LoadScene("Level");
+        SceneManager.LoadScene(1);
+    }
+
+}
